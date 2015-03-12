dbdaf407502c3c9eb458eeae48e41f9ba1c4109e
shd92 <shd92@uw.edu>
2015-03-12 10:08:00 -0700

    Fall Detection App
    Detects a Fall - sounds alarm, vibrates, sends text

---
+++ CppSensors/CppSensors.cpp
@@ -0,0 +1,70 @@
+﻿﻿//CppSensors
+#include "pch.h"
+#include "CppSensors.h"
+#include "Math.h"
+
+using namespace CppSensors;
+using namespace Platform;
+
+//ACCELEROMETER
+CppAcc::CppAcc()
+{
+	this->acc = Accelerometer::GetDefault();
+	this->acc->ReportInterval = 10;
+	this->acc->ReadingChanged += ref new TypedEventHandler < Accelerometer ^, AccelerometerReadingChangedEventArgs ^>(this, &CppAcc::accChanged);
+}
+
+void CppAcc::accChanged(Accelerometer ^sender, AccelerometerReadingChangedEventArgs ^args)
+{
+	this->onReadingChanged(args->Reading->AccelerationX, args->Reading->AccelerationY, args->Reading->AccelerationZ);
+}
+
+CppAcc::~CppAcc()
+{
+
+}
+
+//GYROMETER
+CppGyro::CppGyro()
+{
+	this->gyro = Gyrometer::GetDefault();
+	this->gyro->ReportInterval = this->gyro->MinimumReportInterval;
+	this->gyro->ReadingChanged += ref new TypedEventHandler<Gyrometer ^, GyrometerReadingChangedEventArgs ^>(this, &CppGyro::gyroChanged);
+}
+
+void CppGyro::gyroChanged(Gyrometer ^sender, GyrometerReadingChangedEventArgs ^args)
+{
+	this->onReadingChanged(args->Reading->AngularVelocityX, args->Reading->AngularVelocityY, args->Reading->AngularVelocityZ);
+}
+
+CppGyro::~CppGyro()
+{
+
+}
+
+//COMPASS 
+CppCompass::CppCompass()
+{
+	this->compass = Compass::GetDefault();
+	this->compass->ReportInterval = 10;
+	this->compass->ReadingChanged += ref new TypedEventHandler <Compass ^, CompassReadingChangedEventArgs ^>(this, &CppCompass::compassChanged);
+	n = 0;
+}
+
+void CppCompass::compassChanged(Compass ^sender, CompassReadingChangedEventArgs ^args)
+{
+	double nNew = args->Reading->HeadingMagneticNorth;
+
+	//compassChanged() is just redirecting the function call from the Compass object to our C# class
+	this->onReadingChanged(nNew);
+}
+
+double CppCompass::getN()
+{
+	return n;
+}
+
+CppCompass::~CppCompass()
+{
+
+}
---
+++ CppSensors/CppSensors.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+using namespace Windows::Devices::Sensors;
+using namespace Windows::Foundation;
+
+namespace CppSensors
+{
+	public delegate void AccelerometerEvent(double x, double y, double z);
+	public delegate void GyroscopeEvent(double x, double y, double z);
+	public delegate void CompassEvent(double n);
+
+	public ref class CppAcc sealed
+	{
+	private:
+		Accelerometer ^acc;
+
+	public:
+		CppAcc();
+		event AccelerometerEvent^ onReadingChanged;
+		void accChanged(Accelerometer ^sender, AccelerometerReadingChangedEventArgs ^args);
+		virtual ~CppAcc();
+
+	};
+
+	public ref class CppGyro sealed
+	{
+	private:
+		Gyrometer ^gyro;
+
+	public:
+		CppGyro();
+		event GyroscopeEvent^ onReadingChanged;
+		void gyroChanged(Gyrometer ^sender, GyrometerReadingChangedEventArgs ^args);
+		virtual ~CppGyro();
+	};
+
+
+	public ref class CppCompass sealed
+	{
+	private:
+		Compass ^compass;
+		double n = 0;
+
+	public:
+		CppCompass();
+
+		//Event that we trigger when we have a new reading
+		event CompassEvent^ onReadingChanged;
+
+		//Internal function that gets called by the compass thread when it has a new reading for us
+		void compassChanged(Compass ^sender, CompassReadingChangedEventArgs ^args);
+
+		double getN();
+
+		virtual ~CppCompass();
+	};
+}
